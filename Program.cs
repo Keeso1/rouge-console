e1680a34dffad3b729d3ebf0ab16207b6570186b
Keeso1 <isac.lindh01@gmail.com>
2026-04-02 23:24:54 +0200

    basic event handling

--- Program.cs
+++ Program.cs
@@ -6,15 +6,21 @@ internal class Program
     private static void Main(string[] args)
     {
         var backend = CursesBackend.Load();
-        using var terminal = new Terminal(backend, new TerminalOptions(UseMouse: false));
+        using var terminal = new Terminal(
+            backend,
+            new TerminalOptions(UseMouse: false, SuppressControlKeys: false)
+        );
 
         terminal.Screen.DrawBorder();
         terminal.Screen.Refresh();
 
         foreach (var @event in terminal.Events.Listen(terminal.Screen))
         {
-            if (@event is KeyEvent)
+            terminal.Screen.WriteText($"{@event}\n");
+            if (@event is KeyEvent { Char.Value: 'c' })
+            {
                 break;
+            }
         }
     }
 }
