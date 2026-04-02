55696b207aaeed3391ba7d185f478ded46a0e336
Isac Lindh <isac.lindh@disecta.com>
2026-04-02 17:56:06 +0200

    Assets class added

---
+++ Assets/GameConstants.cs
@@ -0,0 +1,10 @@
+namespace RogueConsole.Assets;
+
+public static class Assets
+{
+    public const char Player = '@';
+    public const char Enemy = 'X';
+    public const char Ground = '.';
+    public const char Bush = 'B';
+    public const char Wall = '¤';
+}
