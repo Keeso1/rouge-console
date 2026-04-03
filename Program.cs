fed3d90fac89f15e5e146af1ddf7b1b06d6e3c32
Alexander Friberg <alexanderfriberg1999@gmail.com>
2026-04-03 11:15:17 +0200

    WIP: main loop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using Sharpie;
+﻿using RogueConsole.World;
+using RogueConsole.Core;
+using System.Drawing;
+using Sharpie;
 using Sharpie.Backend;
 
 internal class Program
@@ -11,16 +14,37 @@ internal class Program
             new TerminalOptions(UseMouse: false, SuppressControlKeys: false)
         );
 
-        terminal.Screen.DrawBorder();
         terminal.Screen.Refresh();
+
+        Canvas canvas = new Canvas(terminal.Screen.Size);
+        TileMap map = new(terminal.Screen.Size.Width, terminal.Screen.Size.Height, canvas);
+        GameState state = new();
+        InputHandler input = new();
 
         foreach (var @event in terminal.Events.Listen(terminal.Screen))
         {
-            terminal.Screen.WriteText($"{@event}\n");
+
+            state.Update();
+            // terminal.Screen.WriteText($"{@event}\n");
+            RenderScreen();
             if (@event is KeyEvent { Char.Value: 'c' })
             {
                 break;
             }
+
+            if (@event is StartEvent)
+            {
+            }
+        }
+
+        void RenderScreen()
+        {
+            map.Render();
+            canvas.DrawOnto(terminal.Screen, new Rectangle(new Point(0, 0), canvas.Size), new Point(0, 0));
+            terminal.Screen.DrawBorder();
+            terminal.Screen.Refresh();
         }
     }
+
+
 }
