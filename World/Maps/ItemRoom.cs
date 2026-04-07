bdde051381fc70336549f417eb542105efe86390
Keeso1 <isac.lindh01@gmail.com>
2026-04-07 13:55:16 +0200

    use x and y to draw chest

--- World/Maps/ItemRoom.cs
+++ World/Maps/ItemRoom.cs
@@ -1,18 +1,18 @@
-using RogueConsole.Utils;
-using RogueConsole.Enums;
-using Sharpie;
 using Microsoft.Extensions.Logging;
+using RogueConsole.Enums;
+using RogueConsole.Utils;
+using Sharpie;
 
 namespace RogueConsole.World.Maps;
 
 public class ItemRoom(Canvas canvas, ILogger logger) : TileMap(canvas)
 {
-	public override void InitMap()
-	{
-		base.InitMap();
-		Active = true;
-		RoomType = RoomTypes.Item;
-		var (c1, c2) = GetCanvasCoords.GetCanvasTopLeft();
-		Set((c1 + 1, c1 + 1), Tile.Chest);
-	}
+    public override void InitMap()
+    {
+        base.InitMap();
+        Active = true;
+        RoomType = RoomTypes.Item;
+        var (c1, c2) = GetCanvasCoords.GetCanvasCenter(canvas);
+        Set((c1 + 1, c2 + 1), Tile.Chest);
+    }
 }
